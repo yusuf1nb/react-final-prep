65481b59bd027ddc7938261c6431afe0077409a8
Yusuf Bakshi <yusufbakshi@Yusufs-MacBook-Pro-391.local>
2023-02-09 21:55:23 +0000

    user stroies but with react

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Posts from "./pages/Posts";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/:id" element={<Posts />}></Route>
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
